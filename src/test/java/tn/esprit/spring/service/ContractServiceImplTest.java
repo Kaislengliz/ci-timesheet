63ae73ba9489bcefbf924aa2a15a9a9feca2f8e0
Firas Salem <firas.salem@esprit.tn>
2020-10-19 15:54:33 +0100

    contrat Service Test

---
+++ src/test/java/tn/esprit/spring/service/ContractServiceImplTest.java
@@ -0,0 +1,27 @@
+package tn.esprit.spring.service;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.spring.services.IContratService;
+import tn.esprit.spring.entities.Contrat;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class ContractServiceImplTest {
+	@Autowired
+	IContratService contatService;
+	
+	@Test
+    public void testRetrieveAllContrat() {
+        List<Contrat> listContrat = contatService.getAllContrats(); 
+       
+        assertEquals(7, listContrat.size());
+    }
+}
